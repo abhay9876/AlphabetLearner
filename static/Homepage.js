1fd8f42b129d63524dc87964b57f6dbeefd09566
gargabhishek0987 <dalchandgarg9627@gmail.com>
2025-01-19 17:50:28 +0530

    added Home page

---
+++ static/Homepage.js
@@ -0,0 +1,7 @@
+document.getElementById('submit-practice').addEventListener('click', function() {  
+    alert('Practice data submitted!');  
+});  
+
+document.getElementById('submit-data').addEventListener('click', function() {  
+    alert('New data submitted!');  
+});
