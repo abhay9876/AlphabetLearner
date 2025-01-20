de640fcbf72fa8e17c7d9109831a14d0d68405c0
abhay9876 <singhabhayrna987@gmail.com>
2025-01-20 18:06:00 +0530

    all pages added

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/practice')
+def practice():
+    return render_template('practice.html')
+
+@app.route('/add_data')
+def add_data():
+    return render_template('add_data.html')
+
+
 if __name__  == '__main__':
     app.run(debug=True)
