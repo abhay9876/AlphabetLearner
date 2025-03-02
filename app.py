c24983458df51ba6da20723846727b5dda806c2c
abhay9876 <singhabhayrna987@gmail.com>
2025-03-02 21:10:22 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template,request , jsonify
-import numpy as np #used to process and save image data as a numpy array.
-import base64 # helps decode the image data sent from the frontend
-from PIL import Image #handles image processing (resizing,grayscale conversion)
-from io import BytesIO #converts byte data into an image format for processing
+import numpy as np 
+import base64 
+from PIL import Image 
+from io import BytesIO 
 import os
 
 
@@ -32,24 +32,68 @@ def add_data():
 def save_drawing():
     try:
         data = request.json.get('image', '')  
+        label = request.json.get('label', '')  
+
+        if not label or not label.isalpha():
+            return jsonify({"error": "Invalid label"}), 400
+        
         if ',' in data:
             image_data = base64.b64decode(data.split(',')[1])  
         else:
             return jsonify({"error": "Invalid image data format"}), 400
 
         image = Image.open(BytesIO(image_data)).convert('L')  
-
-        
         image = image.resize((28, 28))
         img_array = np.array(image) / 255.0  
 
         
-        np.save('saved_data/user_letter.npy', img_array)
+        folder_path = f"saved_data/{label.upper()}"
+        os.makedirs(folder_path, exist_ok=True)
+
+        
+        file_count = len(os.listdir(folder_path)) + 1  
+        file_path = os.path.join(folder_path, f"{label.upper()}_{file_count}.npy")
+
+        np.save(file_path, img_array)
 
         return jsonify({"message": "Drawing saved successfully!"})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
+
+def preprocess_image(image_data):
+    
+    image_data = image_data.split(",")[1]  
+    image_bytes = base64.b64decode(image_data)
+    
+    
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+
+    
+    img = cv2.resize(img, (28, 28))
+    img = img / 255.0  
+
+   
+    img = img.reshape(1, 28, 28, 1)
+    
+    return img
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    image_data = data['image']
+    
+    processed_img = preprocess_image(image_data)
+    
+    
+    prediction = model.predict(processed_img)
+    predicted_label = np.argmax(prediction)  
+    
+    return jsonify({'prediction': str(predicted_label)})
 
 
 if __name__  == '__main__':
